c505c31d1e1400e281e416fe8e7c61603cbff8eb
LabbaiIrfan <labbaiirfan955@gmail.com>
2025-03-23 17:19:17 +0530

    renamed files

--- src/assets/index.js
+++ src/assets/index.js
@@ -50,7 +50,7 @@ import dart from "./tech/dart.jpeg";
 import lightroom from "./tech/lightroom.png";
 import firebase from "./tech/firebase.png";
 import vscode from "./tech/Vscode.jpeg";
-import ascode from "./tech/ascode.png";
+import ascode from "./tech/Ascode.png";
 import bootstrap from "./tech/bootstrap.png";
 import netlify from "./tech/netlify.png";
 import vercel from "./tech/vercel.png";
